5dc8149b59fb1873795f34d14ebbca2c5ba5c11f
Akhila <akhichowdary00@gmail.com>
2026-05-13 13:29:04 +0530

    terraform

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -45,112 +45,112 @@ resource "terraform_data" "mongodb" {
   }
 }
 
-resource "aws_instance" "redis" {
-  ami = local.ami_id
-  instance_type = "t3.micro"
-  subnet_id = local.database_subnet_ids
-  vpc_security_group_ids = [local.redis_sg_id]
+# resource "aws_instance" "redis" {
+#   ami = local.ami_id
+#   instance_type = "t3.micro"
+#   subnet_id = local.database_subnet_ids
+#   vpc_security_group_ids = [local.redis_sg_id]
 
 
-    tags = merge (
+#     tags = merge (
   
-        local.common_tags,
-        {
-            Name = "${local.common_name_suffix}-redis"
-        }
-    )
+#         local.common_tags,
+#         {
+#             Name = "${local.common_name_suffix}-redis"
+#         }
+#     )
 
 
 
-}
+# }
 
-resource "terraform_data" "redis" {
-  triggers_replace = [
-    aws_instance.mongodb.id
-  ]
+# resource "terraform_data" "redis" {
+#   triggers_replace = [
+#     aws_instance.mongodb.id
+#   ]
   
-  connection {
-    type     = "ssh"
-    user     = "ec2-user"
-    password = "DevOps321"
-    host     = aws_instance.redis.private_ip
-  }
+#   connection {
+#     type     = "ssh"
+#     user     = "ec2-user"
+#     password = "DevOps321"
+#     host     = aws_instance.redis.private_ip
+#   }
 
-#terraform copies this file to mongodb server
-  provisioner "file" {
-    source = "bootstrap.sh"
-    destination = "/tmp/bootstrap.sh" 
-  }
+# #terraform copies this file to mongodb server
+#   provisioner "file" {
+#     source = "bootstrap.sh"
+#     destination = "/tmp/bootstrap.sh" 
+#   }
 
-  provisioner "remote-exec" {
-    inline = [
-        "chmod +x /tmp/bootstrap.sh",
-        "sudo sh /tmp/bootstrap.sh redis"
-    ]
-  }
-}
+#   provisioner "remote-exec" {
+#     inline = [
+#         "chmod +x /tmp/bootstrap.sh",
+#         "sudo sh /tmp/bootstrap.sh redis"
+#     ]
+#   }
+# }
 
-resource "aws_instance" "rabbitmq" {
-  ami = local.ami_id
-  instance_type = "t3.micro"
-  subnet_id = local.database_subnet_ids
-  vpc_security_group_ids = [local.rabbbitmq_sg_id]
+# resource "aws_instance" "rabbitmq" {
+#   ami = local.ami_id
+#   instance_type = "t3.micro"
+#   subnet_id = local.database_subnet_ids
+#   vpc_security_group_ids = [local.rabbbitmq_sg_id]
 
 
-    tags = merge (
+#     tags = merge (
   
-        local.common_tags,
-        {
-            Name = "${local.common_name_suffix}-rabbitmq"
-        }
-    )
+#         local.common_tags,
+#         {
+#             Name = "${local.common_name_suffix}-rabbitmq"
+#         }
+#     )
 
 
 
-}
+# }
 
-resource "aws_instance" "rabbitmq" {
-  ami = local.ami_id
-  instance_type = "t3.micro"
-  subnet_id = local.database_subnet_ids
-  vpc_security_group_ids = [local.rabbbitmq_sg_id]
+# resource "aws_instance" "rabbitmq" {
+#   ami = local.ami_id
+#   instance_type = "t3.micro"
+#   subnet_id = local.database_subnet_ids
+#   vpc_security_group_ids = [local.rabbbitmq_sg_id]
 
 
-    tags = merge (
+#     tags = merge (
   
-        local.common_tags,
-        {
-            Name = "${local.common_name_suffix}-rabbitmq"
-        }
-    )
+#         local.common_tags,
+#         {
+#             Name = "${local.common_name_suffix}-rabbitmq"
+#         }
+#     )
 
 
 
-}
+# }
 
-resource "terraform_data" "rabbitmq" {
-  triggers_replace = [
-    aws_instance.rabbitmq.id
-  ]
+# resource "terraform_data" "rabbitmq" {
+#   triggers_replace = [
+#     aws_instance.rabbitmq.id
+#   ]
   
-  connection {
-    type     = "ssh"
-    user     = "ec2-user"
-    password = "DevOps321"
-    host     = aws_instance.rabbitmq.private_ip
-  }
+#   connection {
+#     type     = "ssh"
+#     user     = "ec2-user"
+#     password = "DevOps321"
+#     host     = aws_instance.rabbitmq.private_ip
+#   }
 
-#terraform copies this file to mongodb server
-  provisioner "file" {
-    source = "bootstrap.sh"
-    destination = "/tmp/bootstrap.sh" 
-  }
+# #terraform copies this file to mongodb server
+#   provisioner "file" {
+#     source = "bootstrap.sh"
+#     destination = "/tmp/bootstrap.sh" 
+#   }
 
-  provisioner "remote-exec" {
-    inline = [
-        "chmod +x /tmp/bootstrap.sh",
-        "sudo sh /tmp/bootstrap.sh rabbitmq"
-    ]
-  }
-}
+#   provisioner "remote-exec" {
+#     inline = [
+#         "chmod +x /tmp/bootstrap.sh",
+#         "sudo sh /tmp/bootstrap.sh rabbitmq"
+#     ]
+#   }
+# }
 
