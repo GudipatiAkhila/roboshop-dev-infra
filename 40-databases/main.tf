6f0be6e2cad6a2984d316c8f9241d260c4eecd02
Akhila <akhichowdary00@gmail.com>
2026-05-13 21:43:44 +0530

    terraform

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "mongodb" {
   ami = local.ami_id
   instance_type = "t3.micro"
-  subnet_id = local.database_subnet_ids
+  subnet_id = local.database_subnet_id
   vpc_security_group_ids = [local.mongodb_sg_id]
 
 
